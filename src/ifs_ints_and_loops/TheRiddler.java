9df65913b4c4f252a6d82855b079c1aaec794f11
codeclub <codeclub@smms-rm23-24.smusd.local>
2019-03-06 14:58:30 -0800

    the riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,18 +2,56 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+	
+int score = 0;
+		
+JOptionPane.showMessageDialog(null, "Rules: Please enter one-word, lower-case, answers only. Make sure to spell it right too.");
+String answer = JOptionPane.showInputDialog(null, "What can travel around the world while staying in a corner?");
+		
+if ( answer.equals("stamp")) {
+	score++;
+	JOptionPane.showMessageDialog(null, "Correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong");
+	JOptionPane.showMessageDialog(null, "Correct answer: stamp");
+}
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+//-----------------------------------------------------------------------------------------------------------------------
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+String answer2 = JOptionPane.showInputDialog(null, "What has a head and a tail, but no body?");
 
+if ( answer2.equals("coin")) {
+	score++;
+	JOptionPane.showMessageDialog(null, "Correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong");
+	JOptionPane.showMessageDialog(null, "Correct answer: coin");
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+String answer3 = JOptionPane.showInputDialog(null, "Take off my skin - I won't cry, but you will! What I am?");
+
+if ( answer3.equals("onion")) {
+	score++;
+	JOptionPane.showMessageDialog(null, "Correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong");
+	JOptionPane.showMessageDialog(null, "Correct answer: onion");
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+JOptionPane.showMessageDialog(null, "Score: " + score + "/3" );
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
